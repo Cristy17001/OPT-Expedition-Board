322f080408ef2ccbd4353670c416ce40d75e2055
Cristiano Rocha <up202108813@g.uporto.pt>
2024-03-10 16:09:46 +0000

    refactored TableComponent

--- src/app/components/table/table.tsx
+++ src/app/components/table/table.tsx
@@ -9,6 +9,24 @@ interface TableProps {
     secondsPassed2: number;
 }
 
+function DrawRow(row: any, index: number, isLate: boolean) {
+  return (
+      <tr key={index} className={isLate ? styles.late : undefined}>
+        {row.DutyStartTimeSeconds && <td>{new Date(row.DutyStartTimeSeconds * 1000).toISOString().substring(11, 16)}</td>}
+        {row.DutyEndTimeSeconds && <td>{new Date(row.DutyEndTimeSeconds * 1000).toISOString().substring(11, 16)}</td>}
+        {row.IsDriverPresent && <td>{row.IsDriverPresent}</td>}
+        {row.VehicleNr && <td>{row.VehicleNr}</td>}
+        {row.VehicleLicensePlate && <td>{row.VehicleLicensePlate}</td>}
+        {row.DailyRosterDate && <td>{new Date(row.DailyRosterDate).toLocaleDateString('pt-PT')}</td>}
+        {row.DutyName && <td>{row.DutyName}</td>}
+        {row.DutyEndTime && <td>{row.DutyEndTime}</td>}
+        {row.DutyEndNode && <td>{row.DutyEndNode}</td>}
+        {row.EndLines && <td>{row.EndLines}</td>}
+        {row.EndDriverId1 && <td>{row.EndDriverId1}</td>}
+      </tr>
+  );
+}
+
 const TableComponent: React.FC<TableProps> = ({type, result, secondsPassed, secondsPassed2 }) => (
     <div className={styles.table_container}>
           <table className={styles.table}>
@@ -30,40 +48,13 @@ const TableComponent: React.FC<TableProps> = ({type, result, secondsPassed, seco
             <tbody>
                 {(result as Array<any>).map((row, index) => {
                   if ((type == "entradas" && row.DutyEndTimeSeconds && row.DutyEndTimeSeconds > secondsPassed) || (type == "saidas" && row.DutyStartTimeSeconds && row.DutyStartTimeSeconds > secondsPassed)) {
-                    return (
-                      <tr key={index}>
-                        {row.DutyStartTimeSeconds && <td>{new Date(row.DutyStartTimeSeconds * 1000).toISOString().substring(11, 16)}</td>}
-                        {row.DutyEndTimeSeconds && <td>{new Date(row.DutyEndTimeSeconds * 1000).toISOString().substring(11, 16)}</td>}
-                        {row.IsDriverPresent && <td>{row.IsDriverPresent}</td>}
-                        {row.VehicleNr && <td>{row.VehicleNr}</td>}
-                        {row.VehicleLicensePlate && <td>{row.VehicleLicensePlate}</td>}
-                        {row.DailyRosterDate && <td>{new Date(row.DailyRosterDate).toLocaleDateString('pt-PT')}</td>}
-                        {row.DutyName && <td>{row.DutyName}</td>}
-                        {row.DutyEndTime && <td>{row.DutyEndTime}</td>}
-                        {row.DutyEndNode && <td>{row.DutyEndNode}</td>}
-                        {row.EndLines && <td>{row.EndLines}</td>}
-                        {row.EndDriverId1 && <td>{row.EndDriverId1}</td>}
-                      </tr>
-                    );
+                    return DrawRow(row, index, false);
                   }
                   else if  ((type == "entradas" && row.DutyEndTimeSeconds && row.DutyEndTimeSeconds > secondsPassed2) || (type == "saidas" && row.DutyStartTimeSeconds && row.DutyStartTimeSeconds > secondsPassed2)) {
-                    return (
-                    <tr key={index} className={styles.late}>
-                      {row.DutyStartTimeSeconds && <td>{new Date(row.DutyStartTimeSeconds * 1000).toISOString().substring(11, 16)}</td>}
-                      {row.DutyEndTimeSeconds && <td>{new Date(row.DutyEndTimeSeconds * 1000).toISOString().substring(11, 16)}</td>}
-                      {row.IsDriverPresent && <td>{row.IsDriverPresent}</td>}
-                      {row.VehicleNr && <td>{row.VehicleNr}</td>}
-                      {row.VehicleLicensePlate && <td>{row.VehicleLicensePlate}</td>}
-                      {row.DailyRosterDate && <td>{new Date(row.DailyRosterDate).toLocaleDateString('pt-PT')}</td>}
-                      {row.DutyName && <td>{row.DutyName}</td>}
-                      {row.DutyEndTime && <td>{row.DutyEndTime}</td>}
-                      {row.DutyEndNode && <td>{row.DutyEndNode}</td>}
-                      {row.EndLines && <td>{row.EndLines}</td>}
-                      {row.EndDriverId1 && <td>{row.EndDriverId1}</td>}
-                    </tr>
-                );}
-                return null;
-              })}
+                    return DrawRow(row, index, true);
+                  }
+                  return null;
+                })}
               </tbody>
           </table>
     </div>
