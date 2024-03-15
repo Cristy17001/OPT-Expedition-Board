2c8fd20e17600e2b66c7156995171e0fa1a05bba
Cristiano Rocha <up202108813@g.uporto.pt>
2024-03-15 13:40:43 +0000

    Added icon to the head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Logo from './images/logo.png';
 import "./globals.css";
+
 
 const poppins = Poppins({ weight: ["400", "500", "100", "200", "300", "600", "700", "800", "900"], subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Expedition Board",
   description: "Developed as part of the Projeto Integrador 2023/2024",
+  icons: [
+    {
+      rel: 'icon',
+      sizes: '32x32',
+      url: Logo.src,
+    },
+  ],
 };
 
 export default function RootLayout({
