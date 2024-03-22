c715aa7fa8af535dacc52b2b980e9d768fcb272b
pedro <up202108756@up.pt>
2024-03-22 11:50:19 +0000

    exportConfig and importConfig work

--- src/app/definicoes/page.tsx
+++ src/app/definicoes/page.tsx
@@ -96,13 +96,40 @@ const AppearanceButton: React.FC<{ type: string, title: string, icon: string, fu
     );
 };
 
-const AppearanceSettingsManager: React.FC<{ setLogo: (newLogo: string) => void }> = ({ setLogo }) => {
+
+const ExportConfigButton: React.FC<{ type: string, title: string, icon: string, func: () => void }> = ({ title, icon, func }) => {
+
+    return (
+        <button className={styles['basic-button']} onClick={func}>
+
+            <div>
+                <h2>{title}</h2>
+                <Image src={icon} alt={title} style={{ maxWidth: '50px', maxHeight: '60px', aspectRatio: '1/1' }} />
+            </div>
+        </button>
+    );
+};
+
+const ImportConfigButton: React.FC<{ type: string, title: string, icon: string, func: () => void }> = ({ title, icon, func }) => {
+
+    return (
+        <button className={styles['basic-button']} onClick={func}>
+
+            <div>
+                <h2>{title}</h2>
+                <Image src={icon} alt={title} style={{ maxWidth: '50px', maxHeight: '60px', aspectRatio: '1/1' }} />
+            </div>
+        </button>
+    );
+};
+
+const AppearanceSettingsManager: React.FC<{ setLogo: (newLogo: string) => void, exportConfig: () => void, importConfig: () => void }> = ({ setLogo, exportConfig, importConfig }) => {
     return (
         <div className={styles['appearance-container']}>
             <div className={styles['basic-settings']}>
                 <AppearanceButton title="Trocar Logo" type="test" icon={Logo} func={setLogo}/>
-                <AppearanceButton title="Exportar Configuração" type="test" icon={Download}/>
-                <AppearanceButton title="Importar Configuração" type="test" icon={Upload}/>
+                <ExportConfigButton title="Exportar Configuração" type="test" icon={Download} func={exportConfig}/>
+                <ImportConfigButton title="Importar Configuração" type="test" icon={Upload} func={importConfig}/>
             </div>
         </div>
     );
@@ -136,6 +163,57 @@ const SettingsPage: React.FC = () => {
         });
     }
 
+    const handleExportConfig = () => {
+        const data = {
+            color1: colors[0],
+            color2: colors[1],
+            highlightColor: colors[2],
+            textColor1: colors[3],
+            textColor2: colors[4],
+            logo: logo,
+        };
+
+        const json = JSON.stringify(data, null, 2);
+        const blob = new Blob([json], {type: 'application/json'});
+        const href = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = href;
+        link.download = 'config.json';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
+
+      const handleImportConfig = () => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'application/json'; // Accept only JSON files
+        input.onchange = () => {
+          const file = input.files?.[0];
+          if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+              if (reader.result) {
+                const data = JSON.parse(reader.result.toString());
+                setColors([
+                    data.color1,
+                    data.color2,
+                    data.highlightColor,
+                    data.textColor1,
+                    data.textColor2,
+                ]);
+                setLogo(data.logo);
+              }
+            };
+            reader.onerror = () => {
+              console.error('Error reading file:', reader.error);
+            };
+            reader.readAsText(file); // Read the file as text
+          }
+        };
+        input.click();
+      }
+
 
     const rootStyle = {
         '--color-highlight': userPrefs.highlightColor,
@@ -166,7 +244,7 @@ const SettingsPage: React.FC = () => {
             <form className={styles.settings_container} onSubmit={handleSubmit}>
                 <PageSelector title="Aparência"/>
                 <ColorSelector colors={colors} onColorChange={handleColorChange} />
-                <AppearanceSettingsManager setLogo={handleLogoChange}/>
+                <AppearanceSettingsManager setLogo={handleLogoChange} exportConfig={handleExportConfig} importConfig={handleImportConfig}/>
                 <button className={styles.save_btn} type="submit">Guardar</button>
             </form>
         </>
